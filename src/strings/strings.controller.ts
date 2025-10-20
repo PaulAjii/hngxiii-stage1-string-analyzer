910629204b318d5a3c2e560166495e4874102c70
PaulAjii <paulajii134@gmail.com>
2025-10-20 18:08:49 +0100

    feat: created the analyze strings route

--- src/strings/strings.controller.ts
+++ src/strings/strings.controller.ts
@@ -1,14 +1,18 @@
 import { Controller, Post, Body, Get } from '@nestjs/common';
+import { StringsService } from './strings.service';
+import { ResponseDto } from './dto/response.dto';
 
 @Controller('strings')
 export class StringsController {
+  constructor(private readonly stringsService: StringsService) {}
+
   @Get()
   getHello(): string {
     return 'Hello from Strings';
   }
 
   @Post()
-  analyzeString(@Body('value') value: string): string {
-    return `Received string: ${value}`;
+  analyzeString(@Body('value') value: string): ResponseDto {
+    return this.stringsService.analyzeString(value);
   }
 }
