7a2e547f2611e9ae114ae5dbd2816f87ebb5c100
PaulAjii <paulajii134@gmail.com>
2025-10-20 16:40:04 +0100

    feat: created dtos and validation for input dto

---
+++ src/strings/dto/response.dto.ts
@@ -0,0 +1,13 @@
+export class ResponseDto {
+  id: string;
+  value: string;
+  properties: {
+    length: number;
+    is_palindrome: boolean;
+    unique_characters: number;
+    word_count: number;
+    sha256_hash: string;
+    character_frequency_map: Record<string, number>;
+  };
+  created_at: Date;
+}
