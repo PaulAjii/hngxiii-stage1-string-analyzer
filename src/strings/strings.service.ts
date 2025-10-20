64198558bb11d1b2752e384096619488d5e47e07
PaulAjii <paulajii134@gmail.com>
2025-10-20 18:09:09 +0100

    feat: created the anylyze strings controller

--- src/strings/strings.service.ts
+++ src/strings/strings.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  BadRequestException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { ResponseDto } from './dto/response.dto';
+import { sha256Hash, isPalindrome, uniqueCharacters, wordCount } from './utils';
+import { AnalyzedString } from './interfaces/strings.interface';
 
 @Injectable()
-export class StringsService {}
+export class StringsService {
+  private analyzedStrings: AnalyzedString[] = [];
+  private characterFrequencyMap = (str: string) => {
+    const frequencyMap = new Map<string, number>();
+
+    for (const char of str) {
+      frequencyMap.set(char, (frequencyMap.get(char) || 0) + 1);
+    }
+    return Object.fromEntries(frequencyMap);
+  };
+
+  analyzeString(value: string) {
+    if (!value) {
+      throw new BadRequestException(
+        'Invalid request body or missing "value" field',
+      );
+    }
+
+    if (typeof value !== 'string') {
+      throw new UnprocessableEntityException(
+        'Invalid data type for "value" (must be string)',
+      );
+    }
+
+    const existingString = this.analyzedStrings.find(
+      (str) => str.value === value,
+    );
+    if (existingString) {
+      throw new ConflictException('String already exists in the system');
+    }
+
+    const id = sha256Hash(value.trim());
+
+    const analyzedString: ResponseDto = {
+      id,
+      value,
+      properties: {
+        length: value.length,
+        is_palindrome: isPalindrome(value),
+        unique_characters: uniqueCharacters(value),
+        word_count: wordCount(value),
+        sha256_hash: id,
+        character_frequency_map: this.characterFrequencyMap(value),
+      },
+      created_at: new Date().toISOString(),
+    };
+
+    this.analyzedStrings.push(analyzedString);
+
+    return analyzedString;
+  }
+}
