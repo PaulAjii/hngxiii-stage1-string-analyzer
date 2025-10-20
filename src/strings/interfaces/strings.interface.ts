8b5bba02c529469972e741db614c1821c5827b40
PaulAjii <paulajii134@gmail.com>
2025-10-20 17:22:48 +0100

    feat: created strings type interface

---
+++ src/strings/interfaces/strings.interface.ts
@@ -0,0 +1,13 @@
+export interface AnalyzedString {
+  id: string;
+  value: string;
+  properties: {
+    length: number;
+    is_palindrome: boolean;
+    unique_characters: number;
+    word_count: number;
+    sha256_hash: string;
+    character_frequency_map: Record<string, number>;
+  };
+  created_at: Date;
+}
