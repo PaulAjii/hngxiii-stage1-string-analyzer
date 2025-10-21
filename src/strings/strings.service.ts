eebd8173953384f8ceec0d8ecb7f3539be453b60
PaulAjii <paulajii134@gmail.com>
2025-10-21 15:03:07 +0100

    feat: created the proper filter for the nested properties

--- src/strings/strings.service.ts
+++ src/strings/strings.service.ts
@@ -6,6 +6,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { ResponseDto } from './dto/response.dto';
+import { QueryParamsDto } from './dto/query-params.dto';
 import { sha256Hash, isPalindrome, uniqueCharacters, wordCount } from './utils';
 import { AnalyzedString } from './interfaces/strings.interface';
 
@@ -70,5 +71,55 @@ export class StringsService {
     }
 
     return stringData as ResponseDto;
+  }
+
+  filterAll(query: QueryParamsDto) {
+    let filteredStrings = [...this.analyzedStrings];
+
+    const isPalindrome = query.is_palindrome
+      ? Boolean(query.is_palindrome)
+      : undefined;
+    const maxLength = query.max_length ? Number(query.max_length) : undefined;
+    const minLength = query.min_length ? Number(query.min_length) : undefined;
+    const wordCount = query.word_count ? Number(query.word_count) : undefined;
+    const containsCharacter = query.contains_character;
+
+    if (query.is_palindrome !== undefined) {
+      filteredStrings = filteredStrings.filter(
+        (s) => s?.properties?.is_palindrome === isPalindrome,
+      );
+    }
+
+    if (query.max_length !== undefined) {
+      filteredStrings = filteredStrings.filter(
+        (s) => s?.properties?.length <= maxLength!,
+      );
+    }
+
+    if (query.min_length !== undefined) {
+      filteredStrings = filteredStrings.filter(
+        (s) => s?.properties?.length >= minLength!,
+      );
+    }
+
+    if (query.word_count !== undefined) {
+      filteredStrings = filteredStrings.filter(
+        (s) => s?.properties?.word_count === wordCount!,
+      );
+    }
+
+    if (query.contains_character !== undefined) {
+      filteredStrings = filteredStrings.filter((s) =>
+        s?.value.toLowerCase().includes(containsCharacter.toLowerCase()),
+      );
+    }
+
+    console.log(filteredStrings);
+
+    return {
+      data: filteredStrings,
+      count: filteredStrings.length,
+      filters_applied: { ...query },
+    };
   }
 }
