5e61ae4370c1b4138527690ac717909b5bccbf21
PaulAjii <paulajii134@gmail.com>
2025-10-21 15:15:46 +0100

    feat: added type safety and error checking

--- src/strings/dto/query-params.dto.ts
+++ src/strings/dto/query-params.dto.ts
@@ -1,23 +1,28 @@
-import { IsAlpha, IsBoolean, IsNumber, IsOptional } from 'class-validator';
+import { IsString, IsBoolean, IsNumber, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class QueryParamsDto {
   @IsOptional()
-  @IsBoolean()
-  is_palindrome: boolean;
+  @IsBoolean({ message: 'Invalid query parameter values or types' })
+  @Type(() => Boolean)
+  is_palindrome?: boolean;
 
   @IsOptional()
-  @IsNumber()
+  @IsNumber({}, { message: 'Invalid query parameter values or types' })
+  @Type(() => Number)
   min_length: number;
 
   @IsOptional()
-  @IsNumber()
+  @IsNumber({}, { message: 'Invalid query parameter values or types' })
+  @Type(() => Number)
   max_length: number;
 
   @IsOptional()
-  @IsNumber()
+  @IsNumber({}, { message: 'Invalid query parameter values or types' })
+  @Type(() => Number)
   word_count: number;
 
   @IsOptional()
-  @IsAlpha()
+  @IsString({ message: 'Invalid query parameter values or types' })
   contains_character: string;
 }
