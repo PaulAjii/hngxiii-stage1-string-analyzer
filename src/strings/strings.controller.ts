14275dbce3008816a3077063dc96497f91fe3d8a
PaulAjii <paulajii134@gmail.com>
2025-10-21 16:39:24 +0100

    feat: used the nlp service in the controller

--- src/strings/strings.controller.ts
+++ src/strings/strings.controller.ts
@@ -9,12 +9,16 @@ import {
   HttpCode,
 } from '@nestjs/common';
 import { StringsService } from './strings.service';
+import { NlpService } from './nlp.service';
 import { ResponseDto } from './dto/response.dto';
 import { QueryParamsDto } from './dto/query-params.dto';
 
 @Controller('strings')
 export class StringsController {
-  constructor(private readonly stringsService: StringsService) {}
+  constructor(
+    private readonly stringsService: StringsService,
+    private readonly nlpService: NlpService,
+  ) {}
 
   @Post()
   analyzeString(@Body('value') value: string): ResponseDto {
@@ -24,6 +28,21 @@ export class StringsController {
   @Get()
   filterAll(@Query() query: QueryParamsDto) {
     return this.stringsService.filterAll(query);
+  }
+
+  @Get('filter-by-natural-language')
+  filterByNlp(@Query('query') query: string) {
+    const parsedQuery = this.nlpService.parseQuery(query);
+    const filters = this.filterAll(parsedQuery as QueryParamsDto);
+
+    return {
+      data: filters.data,
+      count: filters.count,
+      interpreted_query: {
+        original: query,
+        parsed_filters: parsedQuery,
+      },
+    };
   }
 
   @Get(':string_value')
