eb92516fed5423defb2465f3e3dd64e4198e4e96
PaulAjii <paulajii134@gmail.com>
2025-10-21 15:03:26 +0100

    feat: removed the get request to the home route

--- src/strings/strings.controller.ts
+++ src/strings/strings.controller.ts
@@ -1,19 +1,20 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Query } from '@nestjs/common';
 import { StringsService } from './strings.service';
 import { ResponseDto } from './dto/response.dto';
+import { QueryParamsDto } from './dto/query-params.dto';
 
 @Controller('strings')
 export class StringsController {
   constructor(private readonly stringsService: StringsService) {}
 
-  @Get()
-  getHello(): string {
-    return 'Hello from Strings';
-  }
-
   @Post()
   analyzeString(@Body('value') value: string): ResponseDto {
     return this.stringsService.analyzeString(value);
+  }
+
+  @Get()
+  filterAll(@Query() query: QueryParamsDto) {
+    return this.stringsService.filterAll(query);
   }
 
   @Get(':string_value')
